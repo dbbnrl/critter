0ea7437f821cd91c94892bbb2d3abc4b35943ad8
Dan Brown <dbbnrl@gmail.com>
2016-12-25 23:45:55 -0500

    Massive cleanup & reorg.

--- dataprep.py
+++ dataprep.py
@@ -1,108 +1,59 @@
 from random import random
 from bisect import bisect
 import itertools
-from keras.preprocessing.image import DirectoryIterator, ImageDataGenerator
+from keras.preprocessing.image import enumerate_images, FileListIterator, MergeIterator
+# from image import enumerate_images
 from sklearn.model_selection import train_test_split
 import numpy as np
 
-# copied from python 3.6:
-def choices(population, weights=None, *, cum_weights=None, k=1):
-        """Return a k sized list of population elements chosen with replacement.
-
-        If the relative weights or cumulative weights are not specified,
-        the selections are made with equal probability.
-
-        """
-        if cum_weights is None:
-            if weights is None:
-                _int = int
-                total = len(population)
-                return [population[_int(random() * total)] for i in range(k)]
-            cum_weights = list(itertools.accumulate(weights))
-        elif weights is not None:
-            raise TypeError('Cannot specify both weights and cumulative weights')
-        if len(cum_weights) != len(population):
-            raise ValueError('The number of weights does not match the population')
-        total = cum_weights[-1]
-        return [population[bisect(cum_weights, random() * total)] for i in range(k)]
-
-class FakeDataGen(object):
-        def random_transform(self, x):
-                return x
-        def standardize(self, x):
-                return x
-
-class Buffered(object):
-        def __init__(self, subiter):
-                self.subiter = subiter
-                self.Xit = iter(())
-                self.Yit = iter(())
-        def __iter__(self):
-                return self
-        def __next__(self):
-                try:
-                        return (next(self.Xit), next(self.Yit))
-                except StopIteration:
-                        (X, Y) = next(self.subiter)
-                        self.Xit = iter(list(X))
-                        self.Yit = iter(list(Y))
-                        return (next(self.Xit), next(self.Yit))
-
-# returns (images, classes) pair
-def load_all_images(dir, **kwargs):
-        gen = DirectoryIterator(dir, FakeDataGen(),
-                                shuffle=False,
-                                batch_size=1000000,
-                                follow_links=True,
-                                classes=['yes', 'no'],
-                                class_mode='binary',
-                                color_mode='grayscale',
-                                **kwargs
-                                )
-        return next(gen)
+# class Buffered(object):
+#         def __init__(self, subiter):
+#                 self.subiter = subiter
+#                 self.Xit = iter(())
+#                 self.Yit = iter(())
+#         def __iter__(self):
+#                 return self
+#         def __next__(self):
+#                 try:
+#                         return (next(self.Xit), next(self.Yit))
+#                 except StopIteration:
+#                         (X, Y) = next(self.subiter)
+#                         self.Xit = iter(list(X))
+#                         self.Yit = iter(list(Y))
+#                         return (next(self.Xit), next(self.Yit))
 
 # returns ([(X, Y)] per dir, (X, Y) test split)
-def load_and_split(config, test_size, **kwargs):
-        trains = []
-        Xtests = []
-        Ytests = []
-        for (dir, weight) in config:
-                print("Loading " + dir)
-                (X, Y) = load_all_images(dir, **kwargs)
+# def load_and_split(config, test_size, **kwargs):
+#         trains = []
+#         Xtests = []
+#         Ytests = []
+#         for (dir, weight) in config:
+#                 print("Loading " + dir)
+#                 (X, Y) = load_all_images(dir, **kwargs)
+#                 (Xtrain, Xtest, Ytrain, Ytest) = train_test_split(X, Y, test_size=test_size)
+#                 trains.append((Xtrain, Ytrain))
+#                 Xtests.extend(Xtest)
+#                 Ytests.extend(Ytest)
+#         return (trains, (np.array(Xtests), np.array(Ytests)))
+
+def prep_data(train_gen, test_gen, config, classes, test_size, **kwargs):
+        train_its = []
+        test_its = []
+        weights = []
+        for (subdir, cls, weight) in config:
+                print("Loading " + subdir)
+                (nb_class, X, Y) = enumerate_images(subdir,
+                                                    classes=classes,
+                                                    dir_class=cls,
+                                                    follow_links=True)
                 (Xtrain, Xtest, Ytrain, Ytest) = train_test_split(X, Y, test_size=test_size)
-                trains.append((Xtrain, Ytrain))
-                Xtests.extend(Xtest)
-                Ytests.extend(Ytest)
-        return (trains, (np.array(Xtests), np.array(Ytests)))
-
-class Weighted(object):
-        def __init__(self, subiters, weights, batch_size):
-                self.subiters = subiters
-                self.cum_weights = list(itertools.accumulate(weights))
-                self.batch_size = batch_size
-        def __iter__(self):
-                return self
-        def __next__(self):
-                its = choices(self.subiters, cum_weights=self.cum_weights, k=self.batch_size)
-                Xs = []
-                Ys = []
-                for it in its:
-                        (X, Y) = next(it)
-                        Xs.append(X)
-                        Ys.append(Y)
-                return (np.array(Xs), np.array(Ys))
-
-def make_weighted_generator(XYpairs, weights, batch_size, **kwargs):
-        # weights = 1.0 / np.asarray(weights)
-        # print("weights: ", weights)
-        iters = [Buffered(ImageDataGenerator(**kwargs).flow(X, Y, batch_size))
-                 for (X, Y) in XYpairs]
-        print("got #iters:", len(iters))
-        return Weighted(iters, weights, batch_size)
-        # return iters[0]
-
-def prep_data(config, target_size, test_size, batch_size, **kwargs):
-        (trains, tests) = load_and_split(config, test_size, target_size=target_size)
-        weights = [weight for (dir, weight) in config]
-        train_gen = make_weighted_generator(trains, weights, batch_size, **kwargs)
-        return (train_gen, tests)
+                train_its.append(FileListIterator(train_gen, Xtrain, Ytrain, nb_class,
+                                                  dim_ordering=train_gen.dim_ordering,
+                                                  **kwargs))
+                test_its.append(FileListIterator(test_gen, Xtest, Ytest, nb_class,
+                                                  dim_ordering=test_gen.dim_ordering,
+                                                 **kwargs))
+                weights.append(weight)
+        trainIt = MergeIterator(train_its, weights=weights)
+        testIt = MergeIterator(test_its, weights=weights)
+        return (trainIt, testIt)
--- train.py
+++ train.py
@@ -1,14 +1,17 @@
+#python3
 # import the necessary packages
 # from sklearn.preprocessing import LabelEncoder
-from keras.models import Sequential
+from keras.models import Sequential, load_model, save_model
 from keras.layers import Activation
 from keras.optimizers import SGD
 from keras.layers import Convolution2D, MaxPooling2D, Dense, Flatten, Dropout
 from keras.utils import np_utils
-# from keras.preprocessing.image import ImageDataGenerator
+from keras.preprocessing.image import ImageDataGenerator, Unbatch
 from imutils import paths
 import numpy as np
-# import argparse
+from matplotlib import pyplot
+from scipy.misc import toimage
+import argparse
 import cv2
 import os
 
@@ -18,41 +21,109 @@ from dataprep import prep_data
 seed = 7
 np.random.seed(seed)
 
-# construct the argument parse and parse the arguments
-# ap = argparse.ArgumentParser()
-# ap.add_argument("-d", "--dataset", required=True,
-	# help="path to input dataset")
-# args = vars(ap.parse_args())
- 
-# define the architecture of the network
-model = Sequential()
+model_file = "crapnet.h5"
 
-model.add(Convolution2D(32, 3, 3, activation="relu", input_shape=(150, 150, 1)))
-model.add(MaxPooling2D(pool_size=(2, 2)))
-
-model.add(Convolution2D(32, 3, 3, activation="relu"))
-model.add(MaxPooling2D(pool_size=(2, 2)))
-
-model.add(Convolution2D(64, 3, 3, activation="relu"))
-model.add(MaxPooling2D(pool_size=(2, 2)))
-
-model.add(Flatten())
-model.add(Dense(64, activation="relu"))
-model.add(Dropout(0.5))
-model.add(Dense(1, activation="sigmoid"))
+classes = ['yes', 'no']
 
 config=[
-    ('data/clear', 50),
-    ('data/nonempty', 50)
+    ('data/clear', 'yes', 0.5),
+    ('data/nonempty', 'no', 0.45),
+    ('data/empty', 'no', 0.05),
     ]
 
-(train_gen, tests) = prep_data(config,
-                               (150, 150),
-                               0.1,
-                               32,
-                               rescale=1.0/255,
-                               horizontal_flip=True
-                               )
+img_size = (120, 160)
+
+#construct the argument parse and parse the arguments
+ap = argparse.ArgumentParser()
+# ap.add_argument("-d", "--dataset", required=True,
+	# help="path to input dataset")
+ap.add_argument("-l", "--load-model", action='store_true')
+ap.add_argument("-t", "--train", action='store_true')
+ap.add_argument("-st", "--show-training", action='store_true')
+ap.add_argument("-sv", "--show-validation", action='store_true')
+ap.add_argument("-c", "--clock-prediction", action='store_true')
+args = ap.parse_args()
+ 
+if args.load_model:
+    model = load_model(model_file)
+else:
+    # define the architecture of the network
+    model = Sequential()
+
+    model.add(Convolution2D(32, 3, 3, activation="relu", input_shape=(img_size + (1,))))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+
+    model.add(Convolution2D(32, 3, 3, activation="relu"))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+
+    model.add(Convolution2D(64, 3, 3, activation="relu"))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+
+    model.add(Flatten())
+    model.add(Dense(64, activation="relu"))
+    model.add(Dropout(0.5))
+    model.add(Dense(1, activation="sigmoid"))
+
+    # train the model using SGD
+    print("[INFO] compiling model...")
+    model.compile(
+            loss="binary_crossentropy",
+            optimizer='rmsprop',
+        metrics=["accuracy"])
+
+print(model.summary())
+
+def maybe_invert(x):
+    if np.random.random() < 0.5:
+        x = 255.0 - x
+    return x
+
+imgen = ImageDataGenerator(rescale=1.0/255,
+                           rotation_range=5,
+                           width_shift_range=0.1,
+                           height_shift_range=0.,
+                           # shear_range=0.,
+                           # zoom_range=0.2,
+                           # preprocessing_function=maybe_invert,
+                           horizontal_flip=True)
+
+(trainIt, testIt) = prep_data(imgen, imgen, config, classes, 0.2,
+                              target_size=img_size,
+                              color_mode='grayscale',
+                              class_mode='binary',
+                              batch_size=32)
+
+def show_training_images(gen, validate=False):
+    pyplot.ion()
+    (fig, axs) = pyplot.subplots(3, 3)
+    axs = axs.flatten()
+    for ax in axs:
+        ax.axis('off')
+    pyplot.show()
+    while True:
+        for i in range(0, 9):
+            if validate:
+                (x, y, yp) = next(gen)
+            else:
+                (x, y) = next(gen)
+            axs[i].imshow(np.squeeze(x), cmap='gray')
+            if (y < 0.5):
+                title = "Match"
+            else:
+                title = "No match"
+            if validate and (y != yp):
+                if (yp < 0.5):
+                    axs[i].set_title("False POS", color="red")
+                else:
+                    axs[i].set_title("False NEG", color="red")
+            else:
+                axs[i].set_title(title, color="black")
+
+
+        pyplot.waitforbuttonpress()
+
+if args.show_training:
+    show_training_images(Unbatch(trainIt))
 
 # partition the data into training and testing splits, using 75%
 # of the data for training and the remaining 25% for testing
@@ -60,20 +131,50 @@ config=[
 # (trainData, testData, trainLabels, testLabels) = train_test_split(
 	# data, labels, test_size=0.25, random_state=42)
 
-# train the model using SGD
-print("[INFO] compiling model...")
-model.compile(
-        loss="binary_crossentropy",
-        optimizer='rmsprop',
-	metrics=["accuracy"])
+if args.train:
+    model.fit_generator(
+            trainIt,
+            samples_per_epoch=2016,
+            nb_epoch=10,
+            validation_data=testIt,
+            nb_val_samples=200)
+    save_model(model, model_file)
 
-print(model.summary())
+# def accum_data(iter, nb_elem):
+#     Xs = []
+#     Ys = []
+#     cnt = 0
+#     while (cnt < nb_elem):
+#         X, Y = next(iter)
+#         Xs.append(X)
+#         Ys.append(Y)
+#         cnt = cnt + len(Y)
+#     return (np.array(Xs), np.array(Ys))
 
-model.fit_generator(
-        train_gen,
-        samples_per_epoch=2000,
-        nb_epoch=50,
-        validation_data=tests)
+class Comparator(object):
+    def __init__(self, model, generator):
+        self.model = model
+        self.generator = generator
+    def __iter__(self):
+        return self
+    def __next__(self):
+        (X, Y) = next(self.generator)
+        Yp = self.model.predict_classes(X)
+        return (X, Y, Yp)
+
+if args.show_validation:
+    [loss, acc] = model.evaluate_generator(testIt, 200)
+    print("LOSS =", loss, "ACC =", acc)
+    show_training_images(Unbatch(Comparator(model, testIt)), validate=True)
+
+if args.clock_prediction:
+    gen = Unbatch(testIt)
+    print("generating...")
+    Xs = []
+    for _ in range(10000):
+        x, y = next(gen)
+        Xs.append(x)
+    model.predict_classes(np.array(Xs))
 
 # show the accuracy on the testing set
 # print("[INFO] evaluating on testing set...")
