12c6fcbb96b1b2ce8a167e4b6f06bece45aa0f7e
Dan Brown <dbbnrl@gmail.com>
2016-12-31 14:18:19 -0500

    Tweaks; export mode.

--- train.py
+++ train.py
@@ -1,13 +1,14 @@
 #python3
 # import the necessary packages
 # from sklearn.preprocessing import LabelEncoder
-from keras.models import Sequential, load_model, save_model
+from keras.models import Sequential, load_model, save_model, model_from_config
 from keras.layers import Activation
 from keras.optimizers import SGD
 from keras.layers import Convolution2D, MaxPooling2D, Dense, Flatten, Dropout
 from keras.callbacks import ModelCheckpoint, TensorBoard
 from keras.utils import np_utils
 from keras.preprocessing.image import ImageDataGenerator, Unbatch
+from keras import backend as K
 from imutils import paths
 import numpy as np
 import matplotlib
@@ -16,6 +17,9 @@ from scipy.misc import toimage
 import argparse
 import cv2
 import os
+import time
+from tensorflow.python.training.training import write_graph
+from tensorflow.python.framework.graph_util import convert_variables_to_constants
 
 from dataprep import prep_data
 
@@ -33,7 +37,8 @@ config=[
     ('data/empty', 'no', 0.05),
     ]
 
-img_size = (240, 320)
+#img_size = (240, 320)
+img_size = (308, 308)
 nb_epoch = 50
 
 #construct the argument parse and parse the arguments
@@ -46,13 +51,15 @@ ap.add_argument("-st", "--show-training", action='store_true')
 ap.add_argument("-sv", "--show-validation", action='store_true')
 ap.add_argument("-sm", "--show-mispredict", action='store_true')
 ap.add_argument("-c", "--clock-prediction", action='store_true')
+ap.add_argument("-e", "--export")
+ap.add_argument("-i", "--import")
 ap.add_argument("-p", "--predict")
 args = ap.parse_args()
  
 if args.show_mispredict:
     args.show_validation = True
 
-load_data = args.train or args.show_training or args.show_validation or args.show_mispredict or args.clock_prediction
+load_data = args.train or args.show_training or args.show_validation or args.show_mispredict
 
 if args.load_model:
     model = load_model(model_file)
@@ -86,11 +93,31 @@ else:
 
 print(model.summary())
 
+# if args.import:
+#     K.set_learning_phase(0)
+
+if args.export:
+    K.set_learning_phase(0)
+    config = model.get_config()
+    weights = model.get_weights()
+    # K.clear_session()
+    model = Sequential.from_config(config)
+    model.set_weights(weights)
+    # saver = Saver()
+    # saver.save(K.get_session(), "tf_checkpoint")
+    graph_def = K.get_session().graph.as_graph_def()
+    frozen_graph = convert_variables_to_constants(K.get_session(), graph_def, [model.output.name[:-2]])
+    write_graph(frozen_graph, "./", args.export, as_text=False)
+    # with open("tfnet.pb", "w") as f:
+        # 1
+        # f.write(str(graph_def))
+    exit()
+
 def preprocess(x):
     # if np.random.random() < 0.5:
     #     x = 255.0 - x
     x -= np.mean(x)
-    # x /= np.std(x)
+    x /= np.std(x)
     # if np.random.random() < 0.5:
     #     x *= -1.
 
@@ -214,13 +241,15 @@ if args.show_validation:
     show_training_images(Unbatch(Comparator(model, testIt)), validate=True)
 
 if args.clock_prediction:
-    gen = Unbatch(testIt)
-    print("generating...")
-    Xs = []
-    for _ in range(1000):
-        x, y = next(gen)
-        Xs.append(x)
-    model.predict_classes(np.array(Xs))
+    shape = (1,) + img_size + (1,)
+    X = np.zeros(shape)
+    print("Running predictions...")
+    start = time.time()
+    for _ in range(100):
+        model.predict_on_batch(X)
+    done = time.time()
+    delta = done - start
+    print("Took", delta / 200.0, "s per.")
 
 if args.predict:
     X = ImageDataGenerator(preprocessing_function=preprocess).flow_from_directory(
